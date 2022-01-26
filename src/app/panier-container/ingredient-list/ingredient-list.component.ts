f65a1d0873c8cb47a50f08ff79f744b9ece1c815
dymafr <erwan1309@hotmail.com>
2022-01-26 17:51:08 +0100

    2 files modified

--- src/app/panier-container/ingredient-list/ingredient-list.component.ts
+++ src/app/panier-container/ingredient-list/ingredient-list.component.ts
@@ -4,10 +4,10 @@ import { Ingredient } from '../../shared/interfaces/ingredient.interface';
 @Component({
   selector: 'app-ingredient-list',
   templateUrl: './ingredient-list.component.html',
-  styleUrls: ['./ingredient-list.component.scss']
+  styleUrls: ['./ingredient-list.component.scss'],
 })
 export class IngredientListComponent implements OnInit {
-  @Input() public ingredients?: Ingredient[];
+  @Input() public ingredients: Ingredient[] | null = null;
 
   constructor() {}
 
--- src/app/panier-container/panier-container.component.ts
+++ src/app/panier-container/panier-container.component.ts
@@ -1,22 +1,22 @@
-import { Component, OnDestroy, OnInit } from "@angular/core";
-import { Subscription } from "rxjs";
-import { Ingredient } from "../shared/interfaces/ingredient.interface";
-import { PanierService } from "../shared/services/panier.service";
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Ingredient } from '../shared/interfaces/ingredient.interface';
+import { PanierService } from '../shared/services/panier.service';
 
 @Component({
-  selector: "app-panier-container",
-  templateUrl: "./panier-container.component.html",
-  styleUrls: ["./panier-container.component.scss"]
+  selector: 'app-panier-container',
+  templateUrl: './panier-container.component.html',
+  styleUrls: ['./panier-container.component.scss'],
 })
 export class PanierContainerComponent implements OnInit, OnDestroy {
-  public ingredients?: Ingredient[] | null;
+  public ingredients: Ingredient[] | null = null;
   public subscription?: Subscription;
 
   constructor(private panierService: PanierService) {}
 
   ngOnInit() {
     this.subscription = this.panierService.ingredients$.subscribe(
-      (ingredients: Ingredient[] | null) => (this.ingredients = ingredients)
+      (ingredients: Ingredient[] | null) => (this.ingredients = ingredients)
     );
   }
 
